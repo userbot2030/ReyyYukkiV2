8d2312c21d44cc5d3935a396d906d10ccfc3203c
userbot2030 <166553732+userbot2030@users.noreply.github.com>
2024-04-24 13:47:32 +0700

    config.py

--- config/config.py
+++ config/config.py
@@ -37,10 +37,10 @@ SONG_DOWNLOAD_DURATION = int(
 )  # Remember to give value in Minutes
 
 # You'll need a Private Group ID for this.
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001969246312"))
 
 # A name for your Music bot.
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
+MUSIC_BOT_NAME = getenv("TeleXMusic")
 
 # Your User ID.
 OWNER_ID = list(
